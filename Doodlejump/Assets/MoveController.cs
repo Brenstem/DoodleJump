aac44f9a07f84e655d4049ce20cc52362fe538c9
Dane Nilsson <dane.nilsson@gmail.com>
2019-01-13 15:25:41 +0100

    Multiple changes
    Removed assets
    player can move left and right
    player bounces off platforms
    player prefab
    platform prefab

---
+++ Doodlejump/Assets/MoveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);   
+    }
+}
