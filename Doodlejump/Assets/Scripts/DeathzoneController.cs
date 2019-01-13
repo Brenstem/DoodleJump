6b91fff14a58bbf94819a6d62dc26fcd4932035f
Dane Nilsson <dane.nilsson@gmail.com>
2019-01-13 16:09:08 +0100

    One way platforms, Player can die

---
+++ Doodlejump/Assets/Scripts/DeathzoneController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathzoneController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Player"))
+        {
+            hitInfo.GetComponent<Health>().TakeDamage(1);
+        }
+    }
+}
---
+++ Doodlejump/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Inpsector variables
+    [SerializeField] int healthPoints;
+
+    // Properties
+    public int hp { get { return healthPoints; } set { healthPoints = value; } }
+    private bool mCanDie = true;
+    public bool CanDie { get { return mCanDie; } set { mCanDie = value; } }
+
+    // Function for making the player take damage
+    public void TakeDamage(int dmg)
+    {
+        hp -= dmg;
+        if (hp <= 0 && CanDie)
+            Die();
+    }
+
+    // Kills the player by deactivating it, destroys any object not the player
+    private void Die()
+    {
+        if (transform.gameObject.CompareTag("Player"))
+            transform.gameObject.SetActive(false);
+
+        else
+            Destroy(this.gameObject);
+    }
+}
