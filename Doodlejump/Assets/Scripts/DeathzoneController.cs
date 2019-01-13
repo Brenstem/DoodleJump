c3d19515a6b8841a1f80c58fbea56ebb8cb036a8
Dane Nilsson <dane.nilsson@gmail.com>
2019-01-13 16:49:48 +0100

    Camera follow script, platforms bounce correctly, deathzone follows player

--- Doodlejump/Assets/Scripts/DeathzoneController.cs
+++ Doodlejump/Assets/Scripts/DeathzoneController.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class DeathzoneController : MonoBehaviour
 {
+    [SerializeField] Transform target;
+    [Range (3, 5)][SerializeField] float yOffset;
+
+    private void Update()
+    {
+        if (target.position.y - yOffset > transform.position.y)
+        {
+            transform.position = new Vector2(transform.position.x, target.position.y - yOffset);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
         if (hitInfo.CompareTag("Player"))
         {
             hitInfo.GetComponent<Health>().TakeDamage(1);
+        }
+        else if (hitInfo.CompareTag("Platform"))
+        {
+            Destroy(hitInfo.gameObject);
         }
     }
 }
---
+++ Doodlejump/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] [Range(0,1)]float smoothspeed;
+    [SerializeField] Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothspeed);
+        transform.position = smoothedPosition;
+    }
+}
+
